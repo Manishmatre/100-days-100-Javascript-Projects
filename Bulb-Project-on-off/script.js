f90de2ac5af71f71c7c79d7738d1bc48c2936fd4
Manish Matre <matremanish52@gmail.com>
2025-01-12 10:40:47 +0530

    some changes

--- Bulb-Project-on-off/script.js
+++ Bulb-Project-on-off/script.js
@@ -1,31 +1,83 @@
-const signal = document.getElementById("signal");
+// const signal = document.getElementById("signal");
+// const bulb = document.getElementById("bulb");
+// const button = document.getElementById("btn");
 
-const bulb = document.getElementById("bulb");
+function changeIMG(){
+   
+    const signal = document.getElementById("signal");
+     const bulbimgs = document.getElementById("bulbimg");
+    const button = document.getElementById("btn");
 
-const button = document.getElementById("btn");
 
-function onBulb() {
-  signal.classList.add("green");
-  signal.classList.remove("red");
-  signal.innerText = "ON";
-
-  bulb.src = "./on bulb.jpg";
-  button.innerText = "Turn OFF Bulb";
-
-  button.style.backgroundColor = "green";
-
-  button.setAttribute("onclick", "offBulb()");
+    if(signal.innerText.match("OFF")){
+      bulbimgs.src = "./on bulb.jpg";
+      signal.classList.add("green"); 
+      signal.classList.remove("red");
+      signal.innerText = "ON";
+      button.innerText = "Turn OFF Bulb";
+      button.style.backgroundColor = "red";
+    }else{
+      bulbimgs.src = "./off bulb.jpg";
+      signal.classList.add("red"); 
+      signal.classList.remove("green");
+      signal.innerText = "OFF";
+      button.innerText = "Turn ON Bulb";
+      button.style.backgroundColor = "green";
+    }
 }
 
-function offBulb() {
-  signal.classList.add("red");
-  signal.classList.remove("green");
-  signal.innerText = "OFF";
 
-  bulb.src = "./off bulb.jpg";
-  button.innerText = "Turn ON Bulb";
+// function dybtn(){
+//    if( signal.innerText = "OFF"){
 
-  button.style.backgroundColor = "red";
+//     //  const signal = document.getElementById("signal");
+//     //  const bulb = document.getElementById("bulb");
+//     //  const button = document.getElementById("btn");
+//     //   button.addEventListener( "click" , ()=> {
+//     //   signal.classList.add("green");
+//     //   signal.classList.remove("red");
+//     //   signal.innerText = "ON";
+//     //   bulb.src = "./on bulb.jpg";
+//     //   button.innerText = "Turn OFF Bulb";
+//     //   button.style.backgroundColor = "green";
+       
+//       // button.setAttribute("onclick", "offBulb()");
+//     })
+    
+//    }else if(signal.innerText = "ON") {
+//       // signal.classList.add("red");
+//       // signal.classList.remove("green");
+//       // signal.innerText = "OFF";
+//       // bulb.src = "./off bulb.jpg";
+//       // button.innerText = "Turn ON Bulb";
+//       // button.style.backgroundColor = "red";
+//    } 
+// }
 
-  button.setAttribute("onclick", "onBulb()");
-}
+
+
+// button.addEventListener( "click" , ()=> {
+//   signal.classList.add("green");
+//   signal.classList.remove("red");
+//   signal.innerText = "ON";
+//   bulb.src = "./on bulb.jpg";
+//   button.innerText = "Turn OFF Bulb";
+//   button.style.backgroundColor = "green";
+   
+//   // button.setAttribute("onclick", "offBulb()");
+// });
+
+
+// function onBulb(){
+
+// }
+
+// function offBulb() {
+//   signal.classList.add("red");
+//   signal.classList.remove("green");
+//   signal.innerText = "OFF";
+//   bulb.src = "./off bulb.jpg";
+//   button.innerText = "Turn ON Bulb";
+//   button.style.backgroundColor = "red";
+//   button.setAttribute("onclick", "onBulb()");
+// }
